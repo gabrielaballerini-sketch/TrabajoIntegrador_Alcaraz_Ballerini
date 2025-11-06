04f10c255856af1afd66c7977dec7ad5133016cd
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-11-06 10:56:11 -0300

    Delete cruce de los Andes

--- CruceDeLosAndes/cruceAndes.js
+++
@@ -1,7 +0,0 @@
-function crearReserva() {
-    const contenedor = document.getElementById("oferta")
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = "¡¡Felicidades!!reservaste tu lugar con éxito"
-    contenedor.appendChild(parrafo)
-
-}
